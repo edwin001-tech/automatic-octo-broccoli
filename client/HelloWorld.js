f9327d174eba6cedc3ae3a930d5103dd6543b535
edwin001-tech <edwinshitakwa8@gmail.com>
2020-08-13 18:03:51 +0300

    MERN setup complete

---
+++ client/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+
+const HelloWorld = () => {
+    return (
+        <div>
+          <h1>Hello, this is MERN STACK!</h1>
+        </div>
+      )
+}
+
+export default hot(module)(HelloWorld)
